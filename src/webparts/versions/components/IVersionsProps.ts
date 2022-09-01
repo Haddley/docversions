de81d9a34db3b0d28cb70974350369e2b2a781e7
Neil Haddley <neil@haddley.com>
2022-09-01 13:14:24 -0500

    Out of the box

---
+++ src/webparts/versions/components/IVersionsProps.ts
@@ -0,0 +1,7 @@
+export interface IVersionsProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/versions/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IVersionsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'VersionsWebPartStrings' {
+  const strings: IVersionsWebPartStrings;
+  export = strings;
+}
